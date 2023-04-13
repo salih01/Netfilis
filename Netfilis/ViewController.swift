f25f28f65caea31d574450327c5e8f405e9630f6
Salih Çakmak <slhckmk01@gmail.com>
2023-04-13 23:03:23 +0300

    Initial Commit

---
+++ Netfilis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netfilis
+//
+//  Created by BilmSoft on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
