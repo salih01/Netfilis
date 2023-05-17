9d8fc38e4ae91cc8a279b5b6ed2f89038e917a28
Salih Çakmak <slhckmk01@gmail.com>
2023-05-17 14:21:18 +0300

    change ViewController

---
+++ Netfilis/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  Netfilis
+//
+//  Created by BilmSoft on 17.04.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var cardView: UIView!
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//MARK: Functions
+    
+
+
+
+}
